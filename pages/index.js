4570facd3e5bf145b83e7f523502858e59cb42bc
Galdino17 <Galdino17@users.noreply.github.com>
2022-10-17 08:15:59 -0300

    Mais um tentativa

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import { useEffect, useState } from 'react'
-//import io from 'Socket.IO-client'
-import io from 'socket.io/node_modules/socket.io-client'
+import io from 'socket.io-client'
 let socket;
 
 const Home = () => {
